454994e2c91d3134f6240c9228bd9f7aa7c48b17
tetsuya.yamamoto <tetsuya.yamamoto@gmail.co.jp>
2024-10-21 15:03:50 +0900

    画像を追加

---
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val messageView: TextView = findViewById(R.id.messageView)
+
+
+    }
+}
